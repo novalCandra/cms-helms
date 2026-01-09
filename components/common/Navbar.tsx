f7ba501d3112862db19c7b0313762aaafb71dcff
novalCandra <dinatanovel5@gmail.com>
2026-01-09 22:14:37 +0700

    feat : ✅ components shadnc ui

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,16 +1,41 @@
-import { HardHat } from "lucide-react";
-import React from "react";
+import { Bell, HardHat, LogOut } from "lucide-react";
+import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export default function Navbar() {
   return (
     <>
-      <nav className="border-b border-border sticky top-0 z-50">
+      <nav className="border-b border-border sticky top-0 z-50 bg-white">
         <div className="container mx-auto px-4 py-4 flex items-center justify-between">
           <div className="flex items-center gap-3">
             <div className="w-10 h-10 rounded-lg flex items-center justify-center bg-blue-500 text-white cursor-pointer">
               <HardHat className="w-6 h-6" />
             </div>
             <span className="text-xl font-bold text-foreground">HelmetHub</span>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <Button variant={"ghost"}>
+              <Bell />
+            </Button>
+
+            <div className="flex items-center cursor-pointer gap-4">
+              <Avatar>
+                <AvatarImage src="" alt="Foto" />
+                <AvatarFallback>MK</AvatarFallback>
+              </Avatar>
+              <div className="hidden sm:block">
+                <p className="text-sm font-medium text-foreground">Mikaela</p>
+                <p className="text-xs text-muted-foreground">
+                  mikaela@gmail.com
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center">
+              <Button variant={"link"}>
+                <LogOut />
+              </Button>
+            </div>
           </div>
         </div>
       </nav>
