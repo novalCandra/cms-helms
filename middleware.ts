76f4afaa4ddae0de91d8fef7da1544fc7b1a28f6
novalCandra <dinatanovel5@gmail.com>
2026-01-16 15:41:29 +0700

    feat :  ✅ Penambahan Package
    Penambahan Package JS Cookies
    - Untuk Menyimpan sebuah token

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const session = request.cookies.get("session");
+  const token = request.cookies.get("token");
 
   //   proteksi semua Router
   if (
     request.nextUrl.pathname.startsWith("/user/home") ||
     request.nextUrl.pathname.startsWith("/admin")
   ) {
-    if (!session) {
+    if (!token) {
       return NextResponse.redirect(new URL("/auth/login", request.url));
     }
   }
