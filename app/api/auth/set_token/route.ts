2fa49f68a7a8b872876cea23b6815b1f04b731bf
novalCandra <dinatanovel5@gmail.com>
2026-01-16 15:44:56 +0700

    feat :✅ perbaikan Route api Cookies

--- app/api/auth/set_token/route.ts
+++ app/api/auth/set_token/route.ts
@@ -1,12 +1,15 @@
-"use server";
 import { NextResponse } from "next/server";
 
-export default function POST() {
+export async function POST(request: Request) {
+  const { token } = await request.json();
+  console.log(token);
   const response = NextResponse.json({ success: true });
 
-  response.cookies.set("session", "user-login", {
-    httpOnly: true,
+  response.cookies.set("token", token, {
+    httpOnly: false,
     path: "/",
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
   });
 
   return response;
