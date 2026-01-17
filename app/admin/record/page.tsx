df2e9d82e89a7d872c0c9c339c9ab396bab3edc6
novalCandra <dinatanovel5@gmail.com>
2026-01-17 21:41:13 +0700

    feat : ✅ page record  search anf filter (admin)

--- app/admin/record/page.tsx
+++ app/admin/record/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   Sidebar,
@@ -13,7 +14,13 @@ import {
 import ConfigSidebar from "../config/configSidebar";
 import Navbar from "../components/Navbar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { ClipboardPenIcon, Search } from "lucide-react";
+import {
+  ChevronUp,
+  ClipboardPenIcon,
+  MoreHorizontal,
+  Search,
+  User2,
+} from "lucide-react";
 import {
   InputGroup,
   InputGroupAddon,
@@ -35,8 +42,105 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import DataRecord from "../config/ConfigDataRecord";
-export default function page() {
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
+import Cookies from "js-cookie";
+type TypeBorrowes = {
+  id: number;
+  users: users;
+  helm: helm;
+  full_name: string;
+  helmet_name: string;
+  borrow_date: string;
+  status: string;
+  return_date: string;
+};
+
+type users = {
+  full_name: string;
+};
+
+type helm = {
+  helmet_name: string;
+};
+
+export default function Page() {
+  const [profile, setProfile] = useState({
+    full_name: "",
+    email: "",
+  });
+  const [loading, setIsLoading] = useState<boolean>(true);
+  const [borrowed, setIsBorrowed] = useState<TypeBorrowes[]>([]);
+  const [search, setIsSearch] = useState("");
+  const [select, setSelect] = useState("all");
+
+  const filteredData = borrowed.filter((item) => {
+    const searchFilter =
+      search.trim() === "" ||
+      `${item.users.full_name} ${item.helm.helmet_name}`
+        .toLowerCase()
+        .includes(search.toLowerCase());
+    const selectStatus = select === "all" || item.status === select;
+    return searchFilter && selectStatus;
+  });
+  async function getProfile() {
+    const token = Cookies.get("token");
+    const apiProfile = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/auth/profile`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "include",
+        cache: "no-cache",
+      },
+    );
+    return apiProfile.json();
+  }
+
+  async function AllBorrowing() {
+    const token = Cookies.get("token");
+    const ApiBorrowing = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/borroed`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "include",
+        cache: "no-cache",
+      },
+    );
+
+    return ApiBorrowing.json();
+  }
+  useEffect(() => {
+    async function BorrowingAll() {
+      const AllBorrowings = await AllBorrowing();
+      const Profile = await getProfile();
+      setProfile({
+        full_name: Profile.data.full_name,
+        email: Profile.data.email,
+      });
+      setIsBorrowed(AllBorrowings.data);
+      setIsLoading(false);
+    }
+    BorrowingAll();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex min-h-screen justify-center items-center mx-auto w-[100vw]">
+        <Spinner className="size-10 text-sky-400" />
+      </div>
+    );
+  }
   return (
     <>
       <Sidebar>
@@ -61,21 +165,27 @@ export default function page() {
             </SidebarGroupContent>
           </SidebarGroup>
         </SidebarContent>
-        <SidebarFooter className="px-5">
+        <SidebarFooter>
           <div className="flex items-center cursor-pointer gap-2.5">
-            <Avatar>
-              <AvatarFallback>MK</AvatarFallback>
-            </Avatar>
-            <div className="flex items-center">
-              <div className="hidden sm:block">
-                <p className="text-sm font-bold">Mikaela</p>
-                <p className="text-muted-foreground">Mikaela@gmail.com</p>
-              </div>
-            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild className="bg-white">
+                <SidebarMenuButton>
+                  <User2 /> {profile.full_name}
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent side="top" className="w-56 rounded-[10px]">
+                <DropdownMenuItem
+                  variant="destructive"
+                  className="w-56 rounded-[10px] cursor-pointer"
+                >
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </SidebarFooter>
       </Sidebar>
-
       <div className="container min-h-full w-[250rem]">
         <Navbar />
         <div className="flex items-center px-10 py-7">
@@ -89,25 +199,24 @@ export default function page() {
                 <div className="flex items-center gap-3.5">
                   <InputGroup className="bg-white rounded-md">
                     <InputGroupInput
-                      id="search"
                       placeholder="Enter You Search"
+                      onChange={(e) => setIsSearch(e.target.value)}
                     />
                     <InputGroupAddon>
                       <Search />
                     </InputGroupAddon>
                   </InputGroup>
-
-                  <Select>
+                  <Select onValueChange={setSelect}>
                     <SelectTrigger className="bg-white">
                       <SelectValue placeholder="All Status" />
                     </SelectTrigger>
                     <SelectContent>
                       <SelectGroup>
                         <SelectItem value="all">All Status</SelectItem>
-                        <SelectItem value="avaible">Avaible</SelectItem>
+                        <SelectItem value="available">Avaible</SelectItem>
+                        <SelectItem value="rented">Retired</SelectItem>
+                        <SelectItem value="maintenance">Maintenance</SelectItem>
                         <SelectItem value="borrowed">Borrowed</SelectItem>
-                        <SelectItem value="maintance">MainTance</SelectItem>
-                        <SelectItem value="retired">Retired</SelectItem>
                       </SelectGroup>
                     </SelectContent>
                   </Select>
@@ -118,22 +227,22 @@ export default function page() {
               <Table>
                 <TableHeader>
                   <TableRow>
+                    <TableHead>Number</TableHead>
                     <TableHead>User</TableHead>
-                    <TableHead>Helmet ID</TableHead>
+                    <TableHead>Helments</TableHead>
                     <TableHead>Borrow Date</TableHead>
                     <TableHead>Return Date</TableHead>
                     <TableHead>Status</TableHead>
-                    <TableHead>Actions</TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {DataRecord.map((item) => (
+                  {filteredData.map((item, index) => (
                     <TableRow key={item.id}>
-                      <TableCell>{item.user}</TableCell>
-                      <TableCell>{item.helment_id}</TableCell>
+                      <TableCell>{index + 1}</TableCell>
+                      <TableCell>{item.users.full_name}</TableCell>
+                      <TableCell>{item.helm.helmet_name}</TableCell>
                       <TableCell>{item.borrow_date}</TableCell>
                       <TableCell>{item.return_date}</TableCell>
-                      <TableCell>{item.action}</TableCell>
                       <TableCell>{item.status}</TableCell>
                     </TableRow>
                   ))}
